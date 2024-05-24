4cea1bfb2299c1c799121be42d8264617a21355e
OlegShav <42734324+OlegShav@users.noreply.github.com>
2024-05-24 06:47:06 +0300

    Add files via upload

---
+++ battleship.py
@@ -0,0 +1,75 @@
+class BoardException(Exception):
+    pass
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за пределы доски"
+
+class BoardUsedException(BoardException):
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку"
+
+class BoardWrongShipException(BoardException):
+    pass
+
+class Ship:
+    def __init__(self, bow, lenght, orient):
+        self.bow = bow
+        self.lenght = lenght
+        self.orient = orient
+        self.lives = lenght
+
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.l):
+            cur_x = self.bow.x
+            cur_y = self.bow.y
+
+            if self.orient == 0:
+                cur_x += i
+
+            elif self.orient == 1:
+                cur_y += i
+
+            ship_dots.append(Dot(cur_x, cur_y))
+            return ship_dots
+
+    def shooten(self, shot):
+        return shot in self.dots
+
+class Dot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return f"Dot({self.x}, {self.y})"
+
+class Board:
+    def __init__(self, hide = False, size = 6):
+        self.hide = hide
+        self.size = size
+
+        self.count = 0
+
+        self.field = [["0"] * size for _ in range(size)]
+
+        self.busy = []
+        self.ships = []
+
+    def __str__(self):
+        res = ""
+        res += "   | 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f'\n{i+1} | ' + ' | '.join(row) + ' |'
+
+        if self.hide:
+            res = res.replace("", 'O')
+        return res
+
+    def out(self, d):
+        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
